f1a03fa90334538b35335316dd1749b21269b921
NoTiShAn <77554492+NoTiShAn@users.noreply.github.com>
2022-06-27 17:47:58 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,13 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDrkogrm68blRLUoM4t-LxqrEQPymdPvGQ",
+  authDomain: "teamvoting-61049.firebaseapp.com",
+  projectId: "teamvoting-61049",
+  storageBucket: "teamvoting-61049.appspot.com",
+  messagingSenderId: "483617208656",
+  appId: "1:483617208656:web:3dec311dcdfa3788fcd455"
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
 export default firebase.database();
